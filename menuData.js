99f628eeee241b63da9c763409ef4cbff588eb6f
TruLunch <123335076+TruLunch@users.noreply.github.com>
2023-04-02 13:27:34 -0400

    Update menuData.js

--- menuData.js
+++ menuData.js
@@ -2,40 +2,41 @@ var data = [{
 	start: numberToDate(getDayInWeek('Sunday', today() + 7)).split(',')[0],
 	end: numberToDate(getDayInWeek('Saturday', today() + 7)).split(',')[0],
 	id: 1,
-	items: [0, 1, 2],
+	items: [0, 2, 4],
 	order: {
 		0: 0,
-		1: 0,
-		2: 0
+		2: 0,
+		4: 0
 	}
 },{
 	start: numberToDate(getDayInWeek('Sunday', today() + 14)).split(',')[0],
 	end: numberToDate(getDayInWeek('Saturday', today() + 14)).split(',')[0],
 	id: 2,
-	items: [1, 2, 0],
+	items: [1, 3, 2],
 	order: {
-		0: 0,
 		1: 0,
-		2: 0
+		2: 0,
+		3: 0
 	}
 },{
 	start: numberToDate(getDayInWeek('Sunday', today() + 21)).split(',')[0],
 	end: numberToDate(getDayInWeek('Saturday', today() + 21)).split(',')[0],
 	id: 3,
-	items: [0, 1, 2],
+	items: [4, 0, 3],
 	order: {
 		0: 0,
-		1: 0,
-		2: 0
+		3: 0,
+		4: 0
 	}
 },{
 	start: numberToDate(getDayInWeek('Sunday', today() + 28)).split(',')[0],
 	end: numberToDate(getDayInWeek('Saturday', today() + 28)).split(',')[0],
 	id: 0,
-	items: [0, 2],
+	items: [2, 3, 1],
 	order: {
-			0: 0,
-			2: 0
+			1: 0,
+			2: 0,
+			3: 0
 		}
 }];
 
@@ -43,7 +44,7 @@ var menu = [
 	{
 		name: "Buffalo Chicken Wrap",
 		id: 0,
-		price: 8.76,
+		price: 8.48,
 		calories: 438,
 		fat: 15,
 		"saturated fat": 6.5,
@@ -57,7 +58,7 @@ var menu = [
 	},{
 		name: "Turkey Pesto Sandwich",
 		id: 1,
-		price: 8.76,
+		price: 8.48,
 		calories: 743,
 		fat: 30.9,
 		"saturated fat": 14.7,
@@ -71,7 +72,7 @@ var menu = [
 	},{
 		name: "Mac n' Cheese",
 		id: 2,
-		price: 8.31,
+		price: 7.98,
 		calories: 605,
 		fat: 24,
 		"saturated fat": 10,
@@ -79,9 +80,37 @@ var menu = [
 		fiber: 16,
 		sugar: 11,
 		protein: 44,
-		tags: ['gluten', 'dairy', 'vegetarian'],
+		tags: ['gluten', 'dairy', 'eggs', 'vegetarian'],
 		image: 'mac-and-cheese.png',
-		ingredients: ['Barilla protein+ Pasta', 'Cheddar cheese', 'Mozzarella cheese', 'Greek yogurt', 'Paprika', 'Garlic powder', 'Salt', 'Pepper', 'Water']
+		ingredients: ['Barilla protein+ Pasta', 'Cheddar cheese', 'Mozzarella cheese', 'Greek yogurt', 'Paprika', 'Garlic powder', 'Salt', 'ground pepper', 'Water']
+	},{
+		name: "Chicken Burrito Bowl",
+		id: 3,
+		price: 8.48,
+		calories: 725,
+		fat: 43,
+		"saturated fat": 15,
+		carbohydrates: 41,
+		fiber: 8,
+		sugar: 4,
+		protein: 47,
+		tags: ['dairy'],
+		image: 'chicken-rice-bowl.png',
+		ingredients: ['yellow onion', 'pepper', 'adobo sauce', 'garlic', 'chicken broth', 'chili powder', 'salt', 'cumin', 'oregano', 'ground pepper', 'chicken', 'white rice', 'iceberg lettuce', 'black beans', 'monterey jack', 'salsa']
+	},{
+		name: "Caesar Salad",
+		id: 4,
+		price: 7.48,
+		calories: 417,
+		fat: 28,
+		"saturated fat": 6,
+		carbohydrates: 30,
+		fiber: 2,
+		sugar: 4,
+		protein: 10,
+		tags: ['dairy', 'eggs', 'vegetarian'],
+		image: 'caesar-salad.png',
+		ingredients: ['mayonnaise', 'garlic', 'anchovy paste', 'worcestershire sauce', 'dijon mustard', 'salt', 'ground pepper', 'croutons', 'iceberg lettuce', 'parmesan cheese']
 	}
 ]
 
